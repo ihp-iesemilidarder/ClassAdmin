bbb592d3de3d5004033de734432c0fb5082e0746
ihp-iesemilidarder <ihp00@iesemilidarder>
2021-11-28 00:32:11 +0100

    development

--- sources/Client.py
+++ sources/Client.py
@@ -16,10 +16,10 @@ class Client:
         return True if len(list(result))>0 else False
 
     # if the new client his ip address is in the clients list
-    def __differentHostAddress(self,ip,clients):
+    def __differentHostAddress(self,ip,nick,clients):
         if clients==None:
             return True
-        result = filter(lambda host: host["address"]==ip,clients)
+        result = filter(lambda host: host["address"]==ip or host["nick"]==nick,clients)
         return True if len(list(result))==0 else False
 
     def registre(self,nick,status,conscent):
@@ -38,7 +38,7 @@ class Client:
                 raise SystemExit"""
 
         # if the clients list is empty and the host is different, this adds the new client in the list
-        if (client==None and status=="CONNECTED") or self.__differentHostAddress(self.address[0],clients):
+        if (client==None and status=="CONNECTED") or self.__differentHostAddress(self.address[0],nick,clients):
             requests.post(f"https://classadmin.server/api/clients",
                 headers={
                     "password":",UPsz)ZfF~ZOh^:YH)o[4P<sF7$jS(",
@@ -54,7 +54,7 @@ class Client:
             return False
         # update client
         else:
-            requests.put(f"https://classadmin.server/api/clients?address={self.address[0]}",
+            requests.put(f"https://classadmin.server/api/clients?address={self.address[0]}&|nick={nick}",
                 headers={
                     "password":",UPsz)ZfF~ZOh^:YH)o[4P<sF7$jS(",
                     "otp":",UPsz)ZfF~ZOh^:YH)o[4P<sF7$jS(",
