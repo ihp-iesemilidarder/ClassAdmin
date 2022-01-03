d9012b5f6ed343c70d4eff82eb998943408ccd1f
ihp-iesemilidarder <ihp00@iesemilidarder.com>
2022-01-03 22:26:53 +0100

    development

--- sources/notification.py
+++ sources/notification.py
@@ -1,15 +1,13 @@
 import os, time
 from sources.utils import Environment, Json
-from plyer import notification
-from tkinter import *
+from notifypy import Notify as notify
 def Notify(title:str,message:str,output=True):
     if(Json(Environment.configuration).print(["notifications"])):
-        notification.notify(
-            app_name="ClassAdmin",
-            app_icon=f"{Environment.media}/images/ClassAdminLogo.ico",
-            title=title,
-            message=message
-        )
+        notification = notify()
+        notification.title = title
+        notification.message = message
+        notification.icon = f"{Environment.media}/images/ClassAdminLogo.png"
+        notification.send()
     if output:
         text = f"{title}: {message}"
         print(text)
