2b0aa0184a3e5c3f5c5019892afccdbe474a3219
ihp-iesemilidarder <ihp00@iesemilidarder.com>
2022-01-05 22:49:58 +0100

    development

--- sources/notification.py
+++ sources/notification.py
@@ -1,13 +1,16 @@
 import os, time
-from sources.utils import Environment, Json
-from notifypy import Notify as notify
+from sources.utils import Environment, Json, logFile
 def Notify(title:str,message:str,output=True):
     if(Json(Environment.configuration).print(["notifications"])):
-        notification = notify()
-        notification.title = title
-        notification.message = message
-        notification.icon = f"{Environment.media}/images/ClassAdminLogo.png"
-        notification.send()
+        try:
+            from notifypy import Notify as notify
+            notification = notify()
+            notification.title = title
+            notification.message = message
+            notification.icon = f"{Environment.media}/images/ClassAdminLogo.png"
+            notification.send()
+        except:
+            None
     if output:
         text = f"{title}: {message}"
         print(text)
