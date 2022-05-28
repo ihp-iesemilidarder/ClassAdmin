9a2f2296a44adbfb75dfec18def2a6c794cfae96
ihp-iesemilidarder <ihp00@iesemilidarder.com>
2022-05-29 00:11:59 +0200

    development

--- Django/api/views/__init__.py
+++ Django/api/views/__init__.py
@@ -1,1 +1,10 @@
+# Author: Ivan Heredia Planas
+# ivanherediaplanas@protonmail.com
+# Licensed by GNU GENERAL PUBLIC LICENSE VERSION 3
+# This file is part of ClassAdmin.
+# ClassAdmin is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+# ClassAdmin is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with ClassAdmin. If not, see <https://www.gnu.org/licenses/>.
+# Copyright 2022 Ivan Heredia Planas
+#
 from .viewStatus import viewStatus
